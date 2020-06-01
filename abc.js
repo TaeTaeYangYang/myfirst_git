b0ddea4ca9acfc646a34fcbad1689c4e0b10872a
전태양 <jeontaeyang@jeontaeyang-ui-MacBookPro.local>
2020-06-02 00:35:55 +0900

    학점

---
+++ abc.js
@@ -0,0 +1,16 @@
+a=92
+b=75
+if(a>=90){
+    console.log("a학점");
+} else if(a>=80){
+    console.log("b학점");
+} else {
+    console.log("c학점");
+}
+if(b>=90){
+    console.log("a학점");
+} else if(b>=80){
+    console.log("b학점");
+} else {
+    console.log("c학점");
+}
