3f65a7887b9b5556fc1d2b3741bfac97e852fd68
전태양 <jeontaeyang@jeontaeyang-ui-MacBookPro.local>
2020-06-02 16:25:27 +0900

    공부
    ㅇㄹㅇ
    node 6월2일.js

---
+++ 6월2일.js
@@ -0,0 +1,41 @@
+//var i=0;
+//while(i<10){
+ //   console.log('coding everybody <br />');
+  //  i++   
+//}
+
+//for(var i = 0; i < 10; i++){
+ //   console.log('coding everyday'+i+'<br />');/
+//}
+
+//for(var i = 0; i < 10; i++){
+  //  if( i === 5 ) {
+    //    break;
+//    }
+ //   console.log('coding everyday'+i+'<br />');
+    
+//}
+
+//for(var i = 0; i < 10; i++) {
+ //   if(i === 5) {
+   //     continue;
+    //}
+    //console.log('coding everyday'+i+'<br />');
+    
+//}
+
+//for(var i = 0; i < 10; i++){
+  //  for(var j = 0; j < 10; j++){
+  //      console.log(i+j)+'<br />';
+        
+   // }
+//}
+
+function numbering(){
+    i = 0;
+    while(i < 10) {
+        console.log(i);
+        i += 1;        
+    }
+}
+numbering();
