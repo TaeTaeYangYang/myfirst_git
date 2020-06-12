82fcdf63431095230c82407428dd26403446c31b
전태양 <jeontaeyang@jeontaeyang-ui-MacBookPro.local>
2020-06-12 13:53:44 +0900

    6월12일

---
+++ MM.js
@@ -0,0 +1,56 @@
+a = [13, -5, -9, 2];
+// num = 0
+
+// for(i = 0 ; i < a.length ; i++){
+//     num = num+a[i]
+// }
+
+// console.log(num/a.length);
+
+// a[0]>a[1]
+
+// a[0]>a[2]
+
+// a[1]>a[2]
+
+
+//최대값
+
+
+// max=0
+
+// for(i =0 ; i < a.length; i++){
+//     if(max < a[i]){
+//         max=a[i];
+//     }
+// }
+
+// console.log(max);
+
+
+//최소값
+
+// min=0
+
+// for(i = 0; i<a.length; i++){
+//     if(min > a[i]){
+//         min=a[i]
+//     }
+// }
+
+// console.log(min);
+
+
+// for(i = 0; i < a.length; i++){
+//     if(a[i]%2 == 0){
+//         console.log(a[i]);
+        
+//     }
+// }
+
+for(i = 0; i < a.length; i++){
+    if(a[i]%2 !== 0){
+        console.log(a[i]);
+        
+    }
+}
